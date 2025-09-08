f5c572442de51bd54a8c31d32af53c00963c36e1
It's Glint <itsglintop@gmail.com>
2025-09-08 12:18:43 +0000

    don't make the preloader's text black the preloader one should be white

--- src/components/preloader.tsx
+++ src/components/preloader.tsx
@@ -5,7 +5,7 @@ const Preloader = () => {
     <div className="fixed inset-0 z-[100] flex items-center justify-center bg-black">
       <div className="relative">
         <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary"></div>
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 [&>a]:text-white">
             <Logo />
         </div>
       </div>
