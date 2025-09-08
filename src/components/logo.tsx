d32617c151748f7b6959b32fd3c532c3f27253f9
It's Glint <itsglintop@gmail.com>
2025-09-08 12:17:51 +0000

    make this text black only on light theme and dark theme it should be whi

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Logo = () => {
   return (
-    <Link href="/" className="text-3xl font-bold font-headline tracking-tighter text-white transition-opacity hover:opacity-80">
+    <Link href="/" className="text-3xl font-bold font-headline tracking-tighter text-foreground transition-opacity hover:opacity-80">
       Nova<span className="text-primary">.</span>
     </Link>
   );
