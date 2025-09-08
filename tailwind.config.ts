d291fe3ca736db43ddcadd5b520fa9d8a2883fd6
It's Glint <itsglintop@gmail.com>
2025-09-08 11:20:01 +0000

    I dont see the animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -85,12 +85,18 @@ export default {
           '0%': { opacity: '0', transform: 'translateY(20px)' },
           '100%': { opacity: '1', transform: 'translateY(0)' },
         },
+        'background-pan': {
+          '0%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+          '100%': { 'background-position': '0% 50%' },
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'fade-in': 'fade-in 1s ease-out forwards',
         'fade-in-up': 'fade-in-up 0.8s cubic-bezier(0.25, 1, 0.5, 1) forwards',
+        'background-pan': 'background-pan 15s ease-in-out infinite',
       },
     },
   },
