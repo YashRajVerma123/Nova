32811db133631423aea8b7d8f651a1b4b55e9faf
It's Glint <itsglintop@gmail.com>
2025-09-08 11:42:19 +0000

    the color circles in light theme are almost invisible so make them a bit

--- src/components/background-animation.tsx
+++ src/components/background-animation.tsx
@@ -1,9 +1,9 @@
 const BackgroundAnimation = () => {
   return (
     <div className="fixed inset-0 -z-10 overflow-hidden">
-      <div className="absolute top-0 -left-4 w-72 h-72 bg-primary/20 rounded-full filter blur-3xl opacity-50 animate-move-circle-1"></div>
-      <div className="absolute top-0 -right-4 w-72 h-72 bg-blue-500/20 rounded-full filter blur-3xl opacity-50 animate-move-circle-2"></div>
-      <div className="absolute bottom-0 left-1/4 w-72 h-72 bg-pink-500/20 rounded-full filter blur-3xl opacity-50 animate-move-circle-3"></div>
+      <div className="absolute top-0 -left-4 w-72 h-72 bg-primary rounded-full filter blur-3xl opacity-20 animate-move-circle-1"></div>
+      <div className="absolute top-0 -right-4 w-72 h-72 bg-blue-500 rounded-full filter blur-3xl opacity-20 animate-move-circle-2"></div>
+      <div className="absolute bottom-0 left-1/4 w-72 h-72 bg-pink-500 rounded-full filter blur-3xl opacity-20 animate-move-circle-3"></div>
     </div>
   );
 }
