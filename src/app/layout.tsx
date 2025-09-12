efc234607bc174ebf8f90c15ca8f56cc038c935b
YASH RAJ VERMA <101271631+YashRajVerma123@users.noreply.github.com>
2025-09-12 13:17:36 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2103302400076966"
+     crossorigin="anonymous"></script>
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-body text-foreground antialiased",
