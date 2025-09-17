eeeb8d12306a105c8bdaba52952284d887f7a71b
It's Glint <itsglintop@gmail.com>
2025-09-17 10:23:20 +0000

    make those color balls more visible and make them fast animated

--- src/components/background-animation.tsx
+++ src/components/background-animation.tsx
@@ -16,9 +16,9 @@ const BackgroundAnimation = () => {
 
   return (
     <div className="fixed inset-0 -z-10 overflow-hidden bg-background">
-      <div className="absolute top-0 left-0 h-96 w-96 bg-primary/20 rounded-full filter blur-3xl opacity-30 animate-move-circle-1"></div>
-      <div className="absolute bottom-0 right-0 h-96 w-96 bg-secondary/20 rounded-full filter blur-3xl opacity-30 animate-move-circle-2"></div>
-      <div className="absolute bottom-1/2 left-1/2 h-80 w-80 bg-primary/10 rounded-full filter blur-2xl opacity-20 animate-move-circle-3"></div>
+      <div className="absolute top-0 left-0 h-96 w-96 bg-primary/20 rounded-full filter blur-3xl opacity-50 animate-move-circle-1"></div>
+      <div className="absolute bottom-0 right-0 h-96 w-96 bg-secondary/20 rounded-full filter blur-3xl opacity-50 animate-move-circle-2"></div>
+      <div className="absolute bottom-1/2 left-1/2 h-80 w-80 bg-primary/10 rounded-full filter blur-2xl opacity-40 animate-move-circle-3"></div>
     </div>
   );
 };
