9c644141559cecb97b1a7cd374a69cfddf8ffa95
It's Glint <itsglintop@gmail.com>
2025-09-18 10:26:20 +0000

    remove that circle loading animation code entirly

--- src/app/(pages)/bulletin/page.tsx
+++ src/app/(pages)/bulletin/page.tsx
@@ -68,11 +68,7 @@ const BulletinPage = () => {
     };
 
     if (loading) {
-        return (
-            <div className="flex h-screen items-center justify-center">
-                <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary"></div>
-            </div>
-        );
+        return null;
     }
     
     return (
